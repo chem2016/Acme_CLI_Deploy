edd728bd160c4900307f63ee7c19a441e04e460d
Haoyu Yu <yuhaoyuapp@gmail.com>
2019-10-10 20:54:58 -0400

    testing User authenticate

--- spec.js
+++ spec.js
@@ -1,21 +1,18 @@
 const { expect } = require('chai');
 const { User } = require('./db').models;
 const { syncAndSeed } = require('./db');
+const jwt = require('jwt-simple');
 
 describe('Our App', () => {
     let seedUsers;
-    // it('true is true', () => {
-    //     expect(true).to.equal(true);
-    // })
-     
     beforeEach(async () => { 
         seedUsers = await syncAndSeed()
     } )
     describe('User authenticate', () => {
         it('successed with correct credientials', async () => {
-            // const { email, password } = seedUsers.moe;
+            const expectedToken = jwt.encode({ id: seedUsers.moe.id }, process.env.SECRET)
             const token = await User.authenticate(seedUsers.moe)
-            expect(true).to.equal(true);
+            expect(token).to.equal(expectedToken);
         })
     })
 })
